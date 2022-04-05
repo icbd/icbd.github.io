ef86965936552d339b272b3897175f440bb69c62
Baodong <wwwicbd@gmail.com>
2022-04-06 00:17:00 +0800

    Add raw_content to Jekyll post through Generator

---
+++ _plugins/raw_content.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Jekyll
+  class RawContent < Generator
+    def generate(site)
+      site.posts.each do |post|
+        post.data['raw_content'] = post.content
+      end
+    end
+  end
+end
